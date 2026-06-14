581ef80b631336277b351c9a011db4c114c9d170
tsazzah <tsazzah@users.noreply.github.com>
2026-06-14 14:47:46 +0000

    deploy: 68af48f449c023a80b510d2aacb8172a65e9365c

--- server/assets/_tanstack-start-manifest_v-ClqphZQ0.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/portfolio/portfolio/src/routes/__root.tsx", children: ["/", "/notable-projects"], preloads: ["/portfolio/assets/index-DYxPEDUd.js"], scripts: [{ attrs: { type: "module", async: true, src: "/portfolio/assets/index-DYxPEDUd.js" } }] }, "/": { filePath: "/home/runner/work/portfolio/portfolio/src/routes/index.tsx", children: void 0, preloads: ["/portfolio/assets/index-ByKlPkhb.js", "/portfolio/assets/Section-BmJvCDtj.js"] }, "/notable-projects": { filePath: "/home/runner/work/portfolio/portfolio/src/routes/notable-projects.tsx", children: void 0, preloads: ["/portfolio/assets/notable-projects-G5kzI4qx.js", "/portfolio/assets/Section-BmJvCDtj.js"] } } });
-export {
-  tsrStartManifest
-};
